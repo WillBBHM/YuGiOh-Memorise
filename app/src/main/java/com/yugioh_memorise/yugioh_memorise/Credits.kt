2e1e9e05a5dcd3b03ad40708e4bfaa7d1ef69f66
WillBBHM <wilfried.benbrahim@gmail.com>
2023-11-25 01:37:10 +0100

    Update app

--- app/src/main/java/com/yugioh_memorise/yugioh_memorise/Credits.kt
+++ app/src/main/java/com/yugioh_memorise/yugioh_memorise/Credits.kt
@@ -1,39 +1,31 @@
 package com.yugioh_memorise.yugioh_memorise
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.content.Intent
 
-class MainActivity : AppCompatActivity() {
+class Credits : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_credits)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val creditButton = findViewById<Button>(R.id.creditButton)
 
-        creditButton.setOnClickListener {
-            val intent = Intent(this, Credits::class.java)
+        val back = findViewById<Button>(R.id.back)
+
+        back.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
-
-        val settingsButton = findViewById<Button>(R.id.settingsButton)
-
-        settingsButton.setOnClickListener {
-            val intent = Intent(this, Settings::class.java)
-            startActivity(intent)
-        }
-
 
 
     }
